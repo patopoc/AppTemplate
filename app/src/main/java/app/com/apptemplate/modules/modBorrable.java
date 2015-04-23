a391b5141fe9ed806e69aebb0d34597d1d564828
steve <admin@mail.com>
2015-04-23 10:23:16 -0400

    acordarse de borrar

---
+++ app/src/main/java/app/com/apptemplate/modules/modBorrable.java
@@ -0,0 +1,7 @@
+package app.com.apptemplate.modules;
+
+/**
+ * Created by steve on 23/04/2015.
+ */
+public class modBorrable {
+}
---
+++ app/src/main/java/app/com/apptemplate/utils/funcBorrable.java
@@ -0,0 +1,7 @@
+package app.com.apptemplate.utils;
+
+/**
+ * Created by steve on 23/04/2015.
+ */
+public class funcBorrable {
+}
